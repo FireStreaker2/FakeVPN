431936d23207df045985afed7783f67321366ed1
FireStreaker2 <michaelccc2019@outlook.com>
2023-12-01 21:24:54 -0800

    simple home page

--- App.tsx
+++ App.tsx
@@ -1,20 +1,36 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { useState } from "react";
+import { useColorScheme } from "react-native";
+import { Stack, TamaguiProvider, Text } from "tamagui";
+import { Switch } from "tamagui";
+
+import config from "./tamagui.config";
 
 export default function App() {
+	const [connected, setConnected] = useState(false);
+	const scheme = useColorScheme();
+
 	return (
-		<View style={styles.container}>
-			<Text>Hello World!</Text>
-			<StatusBar style="auto" />
-		</View>
+		<TamaguiProvider config={config} defaultTheme={scheme}>
+			<Stack
+				display="flex"
+				justifyContent="center"
+				alignItems="center"
+				w="100%"
+			>
+				<Text fontSize="$11">Fake VPN</Text>
+				<Switch
+					size="$7"
+					backgroundColor={connected ? "red" : "gray"}
+					onCheckedChange={() => setConnected(!connected)}
+				>
+					<Switch.Thumb animation="quick" backgroundColor="white" size="$6" />
+				</Switch>
+
+				<Stack alignItems="center">
+					<Text>{connected ? "Connected" : "Disconnected"}</Text>
+					<Text>Your Internet is {connected ? "secure" : "insecure"}</Text>
+				</Stack>
+			</Stack>
+		</TamaguiProvider>
 	);
 }
-
-const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		backgroundColor: "#fff",
-		alignItems: "center",
-		justifyContent: "center",
-	},
-});
